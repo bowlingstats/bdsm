9c87ecadde040efcfce52d4049e1dbde6b486493
Jason Francis <jasonfrancis>
2006-04-03 14:19:34 +0000

    added header.php
    git-svn-id: svn://svn.code.sf.net/p/bdsm/code@16 3dff48b3-900f-0410-87c6-fe2ed2e09ce6

---
+++ testing/header.php
@@ -0,0 +1,8 @@
+<?
+$sql_host = "localhost";//the hostname of your MySQL server, Deafualt is localhost
+$sql_user = "";//the username for your MySQL database
+$sql_pwd = "";//the password of username
+$sql_db = "bdsm";//the name of the database you setup.  Default bdsm
+$conn = mysql_pconnect($sql_host, $sql_user, $sql_pwd);
+$db = mysql_select_db($sql_db);
+?>
