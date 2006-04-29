775bb89f5678504668b91ac40f8a0b6e0cd4713a
Jason Francis <jasonfrancis>
2006-04-29 07:28:14 +0000

    This is the V0.1 release(For real this time).
    git-svn-id: svn://svn.code.sf.net/p/bdsm/code@26 3dff48b3-900f-0410-87c6-fe2ed2e09ce6

--- testing/scoring.js
+++ testing/scoring.js
@@ -1,4 +1,23 @@
 var nonums = /^[0-9]*$/;//regex used to test if something is a number.
+/*
+Bowling Database/Statistics Management
+Copyright (C) 2006 Jason Francis and BD/SM developement team
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 
 /*Game is our grand daddy object.  Everything goes on inside of an instance of Game.*/
 function Game() {
